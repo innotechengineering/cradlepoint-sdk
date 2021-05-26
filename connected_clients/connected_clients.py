ef9d18e9b130ea21e6c7bc935eb7e090bf6635cf
tmcolby <tyson.colby@innotech.engineering>
2021-05-25 17:17:35 -0700

    connected clients using callback

--- connected_clients/connected_clients.py
+++ connected_clients/connected_clients.py
@@ -1,26 +1,15 @@
 from time import sleep
 from csclient import EventingCSClient
-import json
-
-SLEEP_INTERVAL = 10
 
 cp = EventingCSClient('connected_clients')
-while True:
-    #connected_clients = json.loads(cp.get('/status/lan/clients/'))
-    connected_clients = cp.get('/status/lan/clients/')
-    mac_addresses = ''
-    for client in connected_clients:
-        mac_addresses += client['mac'] + ','
-    mac_addresses = mac_addresses.rstrip(',')
+cp.log("1")
+def update_desc(path, connected_clients, *args):
+    mac_addresses = [client['mac'] for client in connected_clients]
+    # mac_addresses = []
+    # for client in connected_clients:
+    #     if client['mac'] not in mac_addresses:
+    #         mac_addresses.append(client['mac'])
+    mac_addresses = ','.join(mac_addresses)
     cp.put('/config/system/desc', mac_addresses)
-    sleep(SLEEP_INTERVAL)
 
-#def update_desc(path, value, *args):
-#    connected_clients = json.loads(value)
-#    mac_addresses = ''
-#    for client in connected_clients:
-#        mac_addresses += client['mac'] + ','
-#    mac_addresses.rstrip(',')
-#    cp.put('/config/system/desc', mac_addresses)
-#
-#cp.register('put', '/status/lan/clients', update_desc)
+cp.register('put', '/status/lan/clients', update_desc)
